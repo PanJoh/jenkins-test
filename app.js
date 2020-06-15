c9b8982f35626d9e65f58766e01d1b1e60ef3e27
Johannes Pandis <johannespandis@gmail.com>
2020-06-15 09:28:46 +0200

    first modification

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, resp) => {
-    resp.type('text/plain').send('Hello World');
+    resp.type('text/plain').send('Hello from express');
 });
 
 app.listen(4000);
