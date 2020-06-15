b8d7e87393c6f4e2a84000e2928fa7dfc7879caa
Johannes Pandis <johannespandis@gmail.com>
2020-06-15 09:46:25 +0200

    4th push

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, resp) => {
-    resp.type('text/plain').send('Hello whatever');
+    resp.type('text/plain').send('Hello World');
 });
 
 app.listen(4000);
